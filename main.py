e8f044039835273ac0f41994aef75addb9589d4b
5nail000 <105508594+5nail000@users.noreply.github.com>
2022-11-12 02:57:23 +0300

    Download comments

--- main.py
+++ main.py
@@ -24,6 +24,17 @@ def download_txt_file(link, file_name, folder = 'books'):
             print (' %s   \r' % file_name, end='\r')
             return True
 
+def parse_comments(url):
+    response = requests.get(url)
+    response.raise_for_status()    
+    soup = BeautifulSoup (response.text, 'lxml')   
+    results = soup.find_all('div', {'class': 'texts'})
+    comments = []
+    for item in results:
+        comment = item.find('span', {'class': 'black'}).text
+        comments.append(comment)
+    return comments
+
 def parse_page(url):
     response = requests.get(url)
     response.raise_for_status()    
@@ -37,10 +48,11 @@ def parse_page(url):
         book_title = item.find_all('td')[1].text
         book_author = item.find_all('td')[2].find('a').text
         book_url = f"https://tululu.org{item.find('a').get('href')}"
-        book_id = book_url[2:-1]
+        book_id = book_url[20:-1]
         book_filename = f'{sanitize_filename(book_title)}({sanitize_filename(book_author)}).txt'
         book_image = f"https://tululu.org{item.find('div', {'class': 'bookimage'}).find('img').get('src')}"
-        books_all.update({book_id : {'url': book_url, 'title': book_title, 'author': book_author, 'book_filename': book_filename, 'image': book_image}})
+        book_comments = parse_comments(book_url)
+        books_all.update({book_id : {'url': book_url, 'title': book_title, 'author': book_author, 'book_filename': book_filename, 'image': book_image, 'comments': book_comments}})
         #print (book_url)
         True
     return books_all
@@ -66,6 +78,7 @@ def main():
             download_txt_file(book_file_url, file_name= file_name)
             file_name = f'{file_name[:-4]}.jpg'
             download_txt_file(books[book]['image'], file_name= file_name, folder= 'images')
+            parse_comments(f'https://tululu.org/b{book}/')
             if index >= quantity: break
 
     print (" "*50, end='\r')
