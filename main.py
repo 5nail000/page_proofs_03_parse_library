37d7344608adf2a8ecb2f5ee54a4d6ab1e937ed5
5nail000 <105508594+5nail000@users.noreply.github.com>
2022-11-12 14:34:31 +0300

    Add argparse

--- main.py
+++ main.py
@@ -3,11 +3,12 @@ from pathvalidate import sanitize_filename, replace_symbol
 from requests.exceptions import HTTPError
 from bs4 import BeautifulSoup
 
+import argparse
 import requests
 import os
 
 
-def download_txt_file(link, file_name, folder = 'books'):
+def download_file(link, file_name, folder = 'books'):
     response = requests.get(link)
     response.raise_for_status()
     try:
@@ -19,8 +20,6 @@ def download_txt_file(link, file_name, folder = 'books'):
 
         with open(Path.cwd()/folder/file_name, 'wb') as file:
             file.write(response.content)
-            print (" "*50, end='\r')
-            print (' %s   \r' % file_name, end='\r')
             return True
 
 def parse_comments(url):
@@ -59,18 +58,16 @@ def parse_book_page(url):
         book_genre.append(genre.text)
 
     return {'id': book_id, 'url': book_url, 'title': book_title, 'author': book_author, 'image': book_image, 'comments': book_comments, 'genre': book_genre}
-    True
 
 
 def parse_page(url):
     response = requests.get(url)
     response.raise_for_status()    
     soup = BeautifulSoup (response.text, 'lxml')
-    
-    #with open('filename.html', "w", encoding='utf_8') as file: file.write(response.text)
-
     results = soup.find('div', {"id": "content"}).find_all('table')
-    books_all = {}
+    last_page = soup.find_all('a', {"class": "npage"})[-1].text
+
+    books_all = {'last_page': int(last_page)}
     for item in results:
         book_title = item.find_all('td')[1].text
         book_author = item.find_all('td')[2].find('a').text
@@ -94,28 +91,38 @@ def check_for_redirect(response):
     if response.history: 
         raise HTTPError
 
-def main():
-    index = 0
-    quantity = 20
+def mass_download_books(start_id = 1, end_id = 100000000):
+    
+    if start_id < 1: start_id = 1
+    if end_id < start_id: end_id = start_id
 
-    for page in range(700):
-        if index >= quantity: break
-        url = f'https://tululu.org/l55/{page+ 1}/'
+    index = 0
+    for page in range(100000000):
+
+        current_page = 1 + page
+        url = f'https://tululu.org/l55/{current_page}/'
         books = parse_page(url)
+        if index >= end_id: break
+        if current_page >= books['last_page']: break
+        books.pop('last_page')
 
         for book in books:
             index += 1
+            if index < start_id: continue
             book_file_url = f'https://tululu.org/txt.php?id={book}'
             file_name = '{:04d}_{}'.format(index, books[book]['book_filename'])
-            download_txt_file(book_file_url, file_name= file_name)
+            download_file(book_file_url, file_name= file_name)
             file_name = f'{file_name[:-4]}.jpg'
-            download_txt_file(books[book]['image'], file_name= file_name, folder= 'images')
+            download_file(books[book]['image'], file_name= file_name, folder= 'images')
             parse_comments(f'https://tululu.org/b{book}/')
-            if index >= quantity: break
+            if index >= end_id: break
 
-    print (" "*50, end='\r')
     print ('Job done')
 
 if __name__ == '__main__':
     
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-start_id", help="start id", type=int, default= 1)
+    parser.add_argument("-end_id", help="end id", type=int, default= 1)
+    args = parser.parse_args()
+    mass_download_books(args.start_id, args.end_id)
