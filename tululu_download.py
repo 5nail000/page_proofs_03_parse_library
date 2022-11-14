d01befee24016e274af960e120a0a6e5e1e8df90
5nail000 <105508594+5nail000@users.noreply.github.com>
2022-11-15 00:23:08 +0300

    PEP (Flake8)

--- tululu_download.py
+++ tululu_download.py
@@ -4,19 +4,17 @@ import time
 from pathlib import Path
 
 import requests
-from requests.exceptions import HTTPError, Timeout
 from bs4 import BeautifulSoup
-from pathvalidate import sanitize_filename, replace_symbol
-
+from pathvalidate import replace_symbol, sanitize_filename
+from requests.exceptions import HTTPError, Timeout
 
 
 def download_file(link, file_name, folder='books'):
-
     read_again = True
     while read_again:
         try:
             response = requests.get(link)
-            response.raise_for_status()            
+            response.raise_for_status()
         except ConnectionError and HTTPError and Timeout:
             time.sleep(2)
         else:
@@ -28,7 +26,6 @@ def download_file(link, file_name, folder='books'):
         True
     else:
         os.makedirs(folder, exist_ok=True)
-
         with open(Path.cwd()/folder/file_name, 'wb') as file:
             file.write(response.content)
             return True
@@ -40,7 +37,7 @@ def parse_comments(url):
     while read_again:
         try:
             response = requests.get(url)
-            response.raise_for_status()            
+            response.raise_for_status()
         except ConnectionError and HTTPError and Timeout:
             time.sleep(2)
         else:
@@ -48,12 +45,12 @@ def parse_comments(url):
 
     soup = BeautifulSoup(response.text, 'lxml')
     results = soup.find_all('div', {'class': 'texts'})
-    
+
     comments = []
     for item in results:
         comment = item.find('span', {'class': 'black'}).text
         comments.append(comment)
-    
+
     return comments
 
 
@@ -63,7 +60,7 @@ def parse_book_page(url):
     while read_again:
         try:
             response = requests.get(url)
-            response.raise_for_status()            
+            response.raise_for_status()
         except ConnectionError and HTTPError and Timeout:
             time.sleep(2)
         else:
@@ -77,7 +74,9 @@ def parse_book_page(url):
         soup.find('div', {"id": "content"}).find('h1').next)[:-2]
     book_author = soup.find('div', {"id": "content"}).find('a').text
 
-    book_image = f"https://tululu.org{soup.find('div', {'class': 'bookimage'}).find('img').get('src')}"
+    book_image = soup.find('div', {'class': 'bookimage'})
+    book_image = book_image.find('img').get('src')
+    book_image = f"https://tululu.org{book_image}"
 
     book_comments = []
     comments = soup.find_all('div', {'class': 'texts'})
@@ -90,22 +89,22 @@ def parse_book_page(url):
     for genre in parse_genres:
         book_genre.append(genre.text)
 
-    return {'id': book_id, 
-            'url': book_url, 
+    return {'id': book_id,
+            'url': book_url,
             'title': book_title,
-            'author': book_author, 
-            'image': book_image, 
-            'comments': book_comments, 
+            'author': book_author,
+            'image': book_image,
+            'comments': book_comments,
             'genre': book_genre}
 
 
 def parse_page(url):
-    
+
     read_again = True
     while read_again:
         try:
             response = requests.get(url)
-            response.raise_for_status()            
+            response.raise_for_status()
         except ConnectionError and HTTPError and Timeout:
             time.sleep(2)
         else:
@@ -127,15 +126,21 @@ def parse_page(url):
 
         book_url = f"https://tululu.org{item.find('a').get('href')}"
         book_id = book_url[20:-1]
-        book_filename = f'{sanitize_filename(book_title)}({sanitize_filename(book_author)}).txt'
-        book_image = f"https://tululu.org{item.find('div', {'class': 'bookimage'}).find('img').get('src')}"
+        sanitize_book_title = sanitize_filename(book_title)
+        sanitize_book_author = sanitize_filename(book_author)
+        book_filename = f'{sanitize_book_title}({sanitize_book_author}).txt'
+
+        book_image = item.find('div', {'class': 'bookimage'})
+        book_image = book_image.find('img').get('src')
+        book_image = f"https://tululu.org{book_image}"
+
         book_comments = parse_comments(book_url)
-        books_all.update({book_id: {'url': book_url, 
-                                    'title': book_title, 
+        books_all.update({book_id: {'url': book_url,
+                                    'title': book_title,
                                     'author': book_author,
-                                    'book_filename': book_filename, 
-                                    'image': book_image, 
-                                    'comments': book_comments, 
+                                    'book_filename': book_filename,
+                                    'image': book_image,
+                                    'comments': book_comments,
                                     'genre': book_genre}})
 
     return books_all
@@ -167,24 +172,24 @@ def mass_download_books(start_id=1, end_id=100000000):
 
         for book in books:
             index += 1
-            if index < start_id: 
+            if index < start_id:
                 continue
-            
+
             book_file_url = f'https://tululu.org/txt.php?id={book}'
-            
+
             file_name = '{:04d}_{}'.format(index, books[book]['book_filename'])
             print(file_name)
-            
+
             download_file(book_file_url, file_name=file_name)
             file_name = f'{file_name[:-4]}.jpg'
             download_file(books[book]['image'],
                           file_name=file_name, folder='images')
-            
+
             parse_comments(f'https://tululu.org/b{book}/')
-            
+
             if index >= end_id:
                 break
-    
+
     return True
 
 
@@ -194,6 +199,6 @@ if __name__ == '__main__':
     parser.add_argument("-start_id", help="start id", type=int, default=1)
     parser.add_argument("-end_id", help="end id", type=int, default=1)
     args = parser.parse_args()
-    
+
     if (mass_download_books(args.start_id, args.end_id)):
         print('Job done')
